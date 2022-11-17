6b3ae269474b042982186b1835fb877c30e4b9c1
joaquin-huergo <jhuergo@udesa.edu.ar>
2022-11-16 21:49:47 -0300

    linkie y arme details movie

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -1,24 +1,35 @@
-window.addEventListener('load', function(){
-    let listContainer = document.querySelector('.lista-productos')
-    let info = ''
-    
-    fetch('https://fakestoreapi.com/products')
-    .then(function(resp){
-        return resp.json()
-    })
-    .then(function(data){
-        for(let i = 0; i < data.length ; i++){
-            info += `
-            <li>
-                <img class='imagen' src='${data[i].image}' />
-                <a href='./details.html?id=${data[i].id}'>${data[i].title}</a>
-            </li>
-            `
-        }
+let query = location.search
+let queryObj = new URLSearchParams(query)
+let id = queryObj.get("id")
+let apikey = "?api_key=282ba42024158eda7c391efcdc7bbf53&language=en-US"
 
-        listContainer.innerHTML = info
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+let url = "https://api.themoviedb.org/3/movie/" + id + apikey
+console.log(url)
+
+fetch(url)
+.then(function(response){
+return response.json()
+}
+)
+.then(function(data){
+console.log(data)
+
+let titulo = document.querySelector(".titulo")
+let sinopsis = document.querySelector(".sinopsis")
+let duracion = document.querySelector(".duracion")
+let estreno = document.querySelector(".Estreno")
+let presentacion = document.querySelector(".presentacion")
+
+titulo.innerText = data.original_title;
+sinopsis.innerText += data.overview;
+duracion.innerHTML += 'Duracion: ' + data.runtime + ' minutos';
+estreno.innerText = 'Fecha de estreno: ' + data.release_date
+
+imagen = '<img src="https://image.tmdb.org/t/p/w500${data.poster_path}" class="imagen" alt=${data.original_title}>'
+
+presentacion.innerHTML = imagen
+}
+)
+.catch(function(error) {
+    console.log("Error: " + error);
 })
